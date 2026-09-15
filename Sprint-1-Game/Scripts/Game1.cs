2abc7789a7a6b480c7fa9629ea605c7aee83a92a
FuchanKay <kazeuma0923@gmail.com>
2026-09-15 12:17:49 -0400

    created monogame library

--- Sprint-1-Game/Scripts/Game1.cs
+++ Sprint-1-Game/Scripts/Game1.cs
@@ -1,33 +1,34 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
 
 namespace Sprint_1_Game;
 
-public class Game1 : Game
+public class Game1 : Core
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    public Game1()
+    private static readonly string name = "Sprint-1-Game";
+    private static readonly int screenWidth = 1280;
+    private static readonly int screenHeight = 720;
+    private static readonly bool isFullScreen = false;
+
+
+    public Game1() : base(name, screenWidth, screenHeight, isFullScreen)
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+
     }
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
+        base.LoadContent();
     }
 
     protected override void Update(GameTime gameTime)
@@ -35,16 +36,12 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
-
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // TODO: Add your drawing code here
 
         base.Draw(gameTime);
     }
