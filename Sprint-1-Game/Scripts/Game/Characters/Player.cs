69e4c4f04ec8e9707dd601b32b96bea9f40080cb
XD1939 <yonganduan1939@gmail.com>
2026-09-22 18:58:11 -0400

    Add some methods to Characters

---
+++ Sprint-1-Game/Scripts/Game/Characters/Player.cs
@@ -0,0 +1,17 @@
+using Microsoft.Xna.Framework;
+using Scripts.GameComponents;
+
+namespace Scripts.Game;
+
+public class Player : ICharacter
+{
+    public Player(Vector2 pos, float speed)
+    {
+        Pos = pos;
+        Speed  = speed;
+        Health = 100;
+    }
+    public int Health { get; set; }
+    public float Speed { get; set; }
+    public Vector2 Pos { get; set; }
+}
